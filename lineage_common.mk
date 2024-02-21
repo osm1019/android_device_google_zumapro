7d1e6820761883538099e047597cc2892f3fd72a
Matt Filetto <matt.filetto@gmail.com>
2024-02-20 17:57:05 -0800

    build pixeldisplayservice rro
    Change-Id: I5bc168c7744d668a218e2ad11c8c069e91d10618
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -37,5 +37,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TetheringOverlay
 
+# PixelDisplayService
+PRODUCT_PACKAGES += \
+    com.android.pixeldisplayservice.auto_generated_rro_product_adevtool__
+
 # Touch
 include hardware/google/pixel/touch/device.mk
