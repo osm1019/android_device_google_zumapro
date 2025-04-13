2d1c2c2d5e45270a880ae4423791165738af648b
Oscar <osm1019@gmail.com>
2025-04-13 11:51:09 -0700

    Infinity

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,13 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_DUP_RULES := true
+
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+SPOOF_FIRST_API_LEVEL_32 := true
+
+
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_LIMIT := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/infinity/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     device/google/zumapro/android.hardware.security.rkp-service.citadel.xml \
     device/google/zumapro/manifest_radio_ds.xml
